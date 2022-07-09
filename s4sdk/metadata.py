10a7e40c6525f132385fd730f8d5507f14310eea
Roy Chotechuang <htraexd@gmail.com>
2022-07-09 18:13:53 +0700

    fix: invalid type instance id

--- s4sdk/metadata.py
+++ s4sdk/metadata.py
@@ -2,7 +2,7 @@ from enum import Enum
 
 
 class ResourceType(Enum):
-    STBL: int = 53690476
+    STBL: int = 570775514
     GFX: int = 1659684250
     DDS: int = 11720834
     SIM_DATA: int = 1415235194
